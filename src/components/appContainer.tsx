06d4dc7ab21740747fe09394c955fca9397535ef
pablo.penovi <pablopenovi@gmail.com>
2022-12-31 13:08:17 -0300

    Agrego middleware de next router para proteger paginas si el usuario no tiene sesión

--- src/components/appContainer.tsx
+++ src/components/appContainer.tsx
@@ -1,16 +1,6 @@
-import { signIn, useSession } from 'next-auth/react';
-import React, { ReactNode, useCallback, useEffect } from 'react';
-import { KeyMap } from '../constants/strings';
-import useStore from '../state/store';
-import LoaderOverlay from './loaderOverlay';
-import { useRouter } from 'next/router';
+import React, { ReactNode } from 'react';
 
 export const DEFAULT_TEST_ID = 'app-container';
-const DEFAULT_CONTAINER_STYLES = `flex flex-col align-middle justify-start min-h-screen text-theme-dark antialiased bg-gradient-to-br from-real-red-300 to-matt-blue-800`
-const PADDING: KeyMap = {
-  'loading': DEFAULT_CONTAINER_STYLES,
-  'default': `${DEFAULT_CONTAINER_STYLES} p-10`
-}
 
 type AppContainerProps = {
   testId?: string,
@@ -21,36 +11,10 @@ const AppContainer = ({
   testId,
   children,
 }: AppContainerProps) => {
-  const router = useRouter()
-  const { isShowSpinner, setShowSpinner } = useStore()
-  const isLoginOrRegisterPage = useCallback(() => (
-    router.pathname === '/auth/login' || router.pathname === '/auth/register'
-  ), [router.pathname])
   
-  const { status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      if (!isLoginOrRegisterPage()) {
-        signIn()
-      }
-    },
-  })
-
-  useEffect(() => {
-    setShowSpinner(!isLoginOrRegisterPage() && status === 'loading')
-  }, [status, isLoginOrRegisterPage, setShowSpinner])
-
-  const getContainerClasses = () => (
-    PADDING[status] || PADDING['default']
-  )
-
   return (
-    <div className={`${getContainerClasses()}`} data-testid={testId || DEFAULT_TEST_ID}>
-      { isShowSpinner ? (
-          <LoaderOverlay testId={'session-spinner'}/>
-        ) : (
-          children
-        )}
+    <div className={`flex flex-col align-middle justify-start min-h-screen p-10 text-theme-dark antialiased bg-gradient-to-br from-real-red-300 to-matt-blue-800`} data-testid={testId || DEFAULT_TEST_ID}>
+      {children}
     </div>
   );
 };
